3f464fc7d06fa5ca2eda0978e6fe5eb9cace1b58
travisbloom <travisbloom@gmail.com>
2014-07-26 19:09:06 -0400

    work to class sign in modal, added h2 for uconn title

--- app/scripts/controllers/modal-controllers.js
+++
@@ -1,4 +0,0 @@
-angular.module('myEasyClass')
-    .controller('signUpModalCtrl', ['$scope', 'userFactory', 'classesFactory', function ($scope, userFactory, classesFactory) {
-        $scope.test = 'testting'
-    }]);
